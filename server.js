960266124011b3837bc0163a735c553c7147fc0a
Diana Nakagawa <drcnakagawa@gmail.com>
2023-03-24 14:24:13 -0300

    added test message

--- server.js
+++ server.js
@@ -3,10 +3,15 @@ const prisma = require("./config/prisma.config");
 
 const userRoutes = require("./routes/user.routes");
 
-const PORT = process.env.PORT || 5000;
+const PORT = 8000;
 
 const app = express();
 app.use(express.json());
+
+// send a message to the root of our API to test that everything is working
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.use("/users", userRoutes);
 
